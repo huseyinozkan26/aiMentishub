44b5cfdebacfa5c62b7c6bd379b4e7d6c6c0777a
root <ozkanhuseyin@yandex.com.tr>
2024-01-21 09:50:00 +0000

    mentishubAiSpeech

---
+++ ai_speech/settings.py
@@ -0,0 +1,163 @@
+#Set-ExecutionPolicy RemoteSigned
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '9=5c*j+v!@r!%)pm1*c0b_c*^ovpkdl*w^!0)fqysl5g2+253-'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'stt_app',
+    'sslserver',
+    'rest_framework',
+    'corsheaders',
+    'rest_framework.authtoken',
+    'pydub'
+]
+
+ALLOWED_HOSTS = ["10.31.1.25","10.31.1.24","localhost", "127.0.0.1","195.214.181.54","etfo.tfo.k12.tr"]
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ai_speech.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ai_speech.wsgi.application'
+
+#cors settings
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',  # for localhost (REACT Default)
+    'http://195.214.181.54/:3000',  # for network 
+    'http://localhost:8080',  # for localhost (Developlemt)
+    'http://10.31.1.24',  # for network (Development)
+    'https://etfo.tfo.k12.tr',  # for network (E-TFO)
+    'http://etfo.tfo.k12.tr',  # for network (E-TFO)
+)
+CORS_ALLOWED_ORIGINS = [
+   'http://localhost:3000',  # for localhost (REACT Default)
+    'http://195.214.181.54/:3000',  # for network 
+    'http://localhost:8080',  # for localhost (Developlemt)
+    'http://10.31.1.24',  # for network (Development)
+    'https://etfo.tfo.k12.tr',  # for network (E-TFO)
+    'http://etfo.tfo.k12.tr',  # for network (E-TFO)
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'localhost',  # for localhost (REACT Default)
+    '195.214.181.54/:3000',  # for network 
+    'localhost:8080',  # for localhost (Developlemt)
+    '10.31.1.24',  # for network (Development)
+    'etfo.tfo.k12.tr',  # for network (E-TFO)
+    'etfo.tfo.k12.tr',  # for network (E-TFO)
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE  = True
+SESSION_COOKIE_SECURE = True
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
